fcc675397d388e8ae400c23938a7331e4f53a465
Mika6413 <michaebony@gmail.com>
2023-09-14 10:14:37 +0200

    Ebauche de la pokemonList

--- vite-project/src/components/PokemonCard.jsx
+++ vite-project/src/components/PokemonCard.jsx
@@ -13,3 +13,16 @@
 // }
 
 // export default PokemonCard
+
+// const pokemonList = [
+//     {
+//       name: "bulbasaur",
+//       imgSrc:
+//         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+//     },
+//     {
+//       name: "mew",
+//     },
+//   ];
+
+//   export default pokemonList
